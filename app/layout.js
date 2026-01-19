50087c06079daa37ed623b4d76629274936602d6
ManicManChaos <eugenedidit8@gmail.com>
2026-01-19 12:44:55 -0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,22 @@
 import "./globals.css";
-import AuthProvider from "../components/AuthProvider";
 
 export const metadata = {
-  title: "Manic Musings of Chaos",
+  manifest: "/manifest.json",
+  icons: {
+    icon: [
+      { url: "/icon/app/icon-32.png", sizes: "32x32", type: "image/png" },
+      { url: "/icon/app/icon-192.png", sizes: "192x192", type: "image/png" }
+    ],
+    apple: [
+      { url: "/icon/app/icon-180.png", sizes: "180x180", type: "image/png" }
+    ]
+  }
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <AuthProvider>{children}</AuthProvider>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
