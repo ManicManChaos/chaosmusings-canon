2a88e1ab040581c043d3d997aa0e9bf045713bce
ManicManChaos <eugenedidit8@gmail.com>
2026-01-19 12:48:45 -0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -3,12 +3,22 @@ import "./globals.css";
 export const metadata = {
   manifest: "/manifest.json",
   icons: {
-    icon: [
-      { url: "/icon/app/icon-32.png", sizes: "32x32", type: "image/png" },
-      { url: "/icon/app/icon-192.png", sizes: "192x192", type: "image/png" }
-    ],
     apple: [
-      { url: "/icon/app/icon-180.png", sizes: "180x180", type: "image/png" }
+      {
+        url: "/icon/app/icon-120.png",
+        sizes: "120x120",
+        type: "image/png"
+      },
+      {
+        url: "/icon/app/icon-167.png",
+        sizes: "167x167",
+        type: "image/png"
+      },
+      {
+        url: "/icon/app/icon-180.png",
+        sizes: "180x180",
+        type: "image/png"
+      }
     ]
   }
 };
