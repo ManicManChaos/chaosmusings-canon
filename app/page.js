63f4ff1383f4f700b8c78333338f965e720680ad
ManicManChaos <eugenedidit8@gmail.com>
2026-01-19 22:59:26 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,20 +1,8 @@
 // app/page.js
 "use client";
 
-import { useState } from "react";
-import OpeningFlow from "@/components/OpeningFlow";
 import AppShell from "@/components/AppShell";
 
 export default function Page() {
-  const [unlocked, setUnlocked] = useState(false);
-
-  return (
-    <>
-      {!unlocked ? (
-        <OpeningFlow onDone={() => setUnlocked(true)} />
-      ) : (
-        <AppShell />
-      )}
-    </>
-  );
+  return <AppShell />;
 }
