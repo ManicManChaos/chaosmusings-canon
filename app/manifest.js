7d9875ff091c08eed97b611ac870880e6acbee76
ManicManChaos <eugenedidit8@gmail.com>
2026-01-19 19:19:31 -0600

    Create manifest.js

---
+++ app/manifest.js
@@ -0,0 +1,12 @@
+export default {
+  name: "Chaos Musings",
+  short_name: "Chaos",
+  display: "standalone",
+  background_color: "#050806",
+  theme_color: "#050806",
+  icons: [
+    { src: "/app/icon-120.png", sizes: "120x120", type: "image/png" },
+    { src: "/app/icon-167.png", sizes: "167x167", type: "image/png" },
+    { src: "/app/icon-180.png", sizes: "180x180", type: "image/png" }
+  ]
+};
