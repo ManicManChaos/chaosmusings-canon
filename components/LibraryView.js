852cee70d6ac98b79b6331b07dd67575f4548c20
ManicManChaos <eugenedidit8@gmail.com>
2026-01-20 06:23:17 -0600

    Update LibraryView.js

--- components/LibraryView.js
+++ components/LibraryView.js
@@ -1,9 +1,27 @@
 "use client";
 
+/**
+ * LIBRARY VIEW (CANON)
+ * - NOT in sidebar
+ * - Accessed from glyphs / context / future routing
+ * - Houses:
+ *   • App Info Book
+ *   • System Map
+ *   • Glyph Legend
+ *   • Settings Book (programmable baselines)
+ */
+
 export default function LibraryView() {
   return (
     <section className="card">
       <div className="cardHead">LIBRARY</div>
+
+      <div className="cardBody">
+        <div className="libraryItem">APP INFO BOOK</div>
+        <div className="libraryItem">SYSTEM MAP</div>
+        <div className="libraryItem">GLYPH LEGEND</div>
+        <div className="libraryItem">SETTINGS</div>
+      </div>
     </section>
   );
 }
