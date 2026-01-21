473957770a066f9a2d52eb6023640d0699d56fb8
ManicManChaos <eugenedidit8@gmail.com>
2026-01-21 17:30:48 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,16 @@
 // app/page.js
 "use client";
 
+import { useState } from "react";
 import AppShell from "@/components/AppShell";
+import OpeningFlow from "@/components/OpeningFlow";
 
 export default function Page() {
+  const [ready, setReady] = useState(false);
+
+  if (!ready) {
+    return <OpeningFlow onDone={() => setReady(true)} />;
+  }
+
   return <AppShell />;
 }
