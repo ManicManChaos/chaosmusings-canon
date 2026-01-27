8bd9dcc69e68d34533d81bfa8e4af52a3603a7c5
ManicManChaos <eugenedidit8@gmail.com>
2026-01-26 19:58:46 -0600

    Rename Sidebar import to SidebarView

--- components/AppShell.js
+++ components/AppShell.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useMemo, useState } from "react";
-import Sidebar from "./Sidebar";
+import Sidebar from "./SidebarView`";
 import Weave from "./Weave";
 import OpeningFlow from "./OpeningFlow";
 
